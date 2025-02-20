ed988811369e334860f8226b411e09a9a36212a1
tengis <tengisuuganbaatar123@gmail.com>
2025-02-20 10:55:14 +0800

    ehnih

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+   <Header/>
+
+
+
+  </>
+     
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+function Header(){
+    return(
+        <>
+        <div className='h1'>
+        <h1>Hi</h1>
+        <h1>I'm</h1>
+        </div>
+        <h1>Tengis</h1>
+
+        </>
+    )
+}
+export default Header
