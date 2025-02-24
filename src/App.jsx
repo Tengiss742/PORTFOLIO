2cbedf958748af906f098129a6511790d0ba3554
tengis <tengisuuganbaatar123@gmail.com>
2025-02-24 12:35:18 +0800

    2.24

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header.jsx'
+import Nav from './components/nav.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-   <Header/>
+  <div>
+    <Nav/>
+    <Header/>
+   
+  </div>
+   
 
 
 
---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+import './nav.css'
+function Nav(){
+    return(
+        <>
+         <nav className='navbar'>
+            <div className='navdiv'>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Gaming Experience</a></li>
+                <li><a href="#">Social</a></li>
+            </div>
+         </nav>
+        
+
+
+
+        </>
+    )
+}
+export default Nav
