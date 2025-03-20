0c44ea8f2b47e2d5041085c9952ba6d563b36472
tengis <tengisuuganbaatar123@gmail.com>
2025-03-20 10:53:26 +0800

    v2.7

--- src/components/gaming.jsx
+++ src/components/gaming.jsx
@@ -7,6 +7,10 @@ function Gaming(){
                 <div className='mostt'>
                     <h1 className='most'>Most Played Games</h1>
                 </div>
+                <div className='scroll-container'>
+                   <img src="https://clipartcraft.com/images/minecraft-logo-png-2.png" alt=""width="40px"/>
+                   <img src="https://i.pinimg.com/originals/5e/d3/e5/5ed3e5f342657fa99dd1869fab1c59f0.png" alt=""width="100px"/>
+                </div>
             </div>
          </section>
         </>
