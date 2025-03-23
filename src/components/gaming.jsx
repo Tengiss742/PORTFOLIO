ae66969ea4afce2685f1fe5c9905ab7a94871cbf
tengis <tengisuuganbaatar123@gmail.com>
2025-03-23 22:22:12 +0800

    v2.8

--- src/components/gaming.jsx
+++ src/components/gaming.jsx
@@ -7,14 +7,25 @@ function Gaming(){
                 <div className='mostt'>
                     <h1 className='most'>Favorite Games </h1>
                 </div>
-                <div className='scroll-container'>
-                   <img src="https://clipartcraft.com/images/minecraft-logo-png-2.png" alt=""width="40px"/>
-                   <img src="https://i.pinimg.com/originals/5e/d3/e5/5ed3e5f342657fa99dd1869fab1c59f0.png" alt=""width="100px"/>
-                   <img src="https://static.vecteezy.com/system/resources/previews/024/983/597/original/valorant-logo-transparent-free-png.png" alt="" width="60px" />
-                   <img src="https://wallpapercave.com/wp/wp4081866.png" alt=""width="60px"/>
-                    <img src="https://th.bing.com/th/id/R.802d1112c9b194b39af9d69162a77402?rik=SNyGICruK8kEjA&pid=ImgRaw&r=0" alt=""width="80px"/>
-                    <img src="https://th.bing.com/th/id/R.f5bd93fd40b2baddc1f5f3eed832e869?rik=yAi14gcLhgMoyA&riu=http%3a%2f%2fvignette1.wikia.nocookie.net%2frealsteel%2fimages%2fa%2fa8%2fImg-2208965-1-Atom_Real_Steel_Boxer.png%2frevision%2flatest%3fcb%3d20140915183810&ehk=xnfjr9Lc9FrA7ibKJQwmI6HfzlryL%2bAop0RDUReuHlc%3d&risl=&pid=ImgRaw&r=0" alt=""width="60px" />
-                    <img src="https://www.pinclipart.com/picdir/big/84-848818_soul-knight-alchemist-soul-knight-all-characters-clipart.png" alt=""width="60px"/>
+                <div className='slider'>
+                    <div className='slide-tracker'>
+                        <div className='slide'><img src="https://clipartcraft.com/images/minecraft-logo-png-2.png" alt=""width="40px"/></div> 
+                        <div className='slide'> <img src="https://i.pinimg.com/originals/5e/d3/e5/5ed3e5f342657fa99dd1869fab1c59f0.png" alt=""width="100px"/></div> 
+                         <div className='slide'>   <img src="https://static.vecteezy.com/system/resources/previews/024/983/597/original/valorant-logo-transparent-free-png.png" alt="" width="60px" /></div>
+                         <div className='slide'><img src="https://th.bing.com/th/id/R.802d1112c9b194b39af9d69162a77402?rik=SNyGICruK8kEjA&pid=ImgRaw&r=0" alt=""width="80px"/></div>
+                         <div className='slide'>
+                         <img src="https://th.bing.com/th/id/R.f5bd93fd40b2baddc1f5f3eed832e869?rik=yAi14gcLhgMoyA&riu=http%3a%2f%2fvignette1.wikia.nocookie.net%2frealsteel%2fimages%2fa%2fa8%2fImg-2208965-1-Atom_Real_Steel_Boxer.png%2frevision%2flatest%3fcb%3d20140915183810&ehk=xnfjr9Lc9FrA7ibKJQwmI6HfzlryL%2bAop0RDUReuHlc%3d&risl=&pid=ImgRaw&r=0" alt=""width="60px" /></div>
+                         <div className='slide'> <img src="https://www.pinclipart.com/picdir/big/84-848818_soul-knight-alchemist-soul-knight-all-characters-clipart.png" alt=""width="60px"/></div>
+                         <div className='slide'><img src="https://clipartcraft.com/images/minecraft-logo-png-2.png" alt=""width="40px"/></div> 
+                        <div className='slide'> <img src="https://i.pinimg.com/originals/5e/d3/e5/5ed3e5f342657fa99dd1869fab1c59f0.png" alt=""width="100px"/></div> 
+                         <div className='slide'>   <img src="https://static.vecteezy.com/system/resources/previews/024/983/597/original/valorant-logo-transparent-free-png.png" alt="" width="60px" /></div>
+                         <div className='slide'><img src="https://th.bing.com/th/id/R.802d1112c9b194b39af9d69162a77402?rik=SNyGICruK8kEjA&pid=ImgRaw&r=0" alt=""width="80px"/></div>
+                         <div className='slide'>
+                         <img src="https://th.bing.com/th/id/R.f5bd93fd40b2baddc1f5f3eed832e869?rik=yAi14gcLhgMoyA&riu=http%3a%2f%2fvignette1.wikia.nocookie.net%2frealsteel%2fimages%2fa%2fa8%2fImg-2208965-1-Atom_Real_Steel_Boxer.png%2frevision%2flatest%3fcb%3d20140915183810&ehk=xnfjr9Lc9FrA7ibKJQwmI6HfzlryL%2bAop0RDUReuHlc%3d&risl=&pid=ImgRaw&r=0" alt=""width="60px" /></div>
+                         <div className='slide'> <img src="https://www.pinclipart.com/picdir/big/84-848818_soul-knight-alchemist-soul-knight-all-characters-clipart.png" alt=""width="60px"/></div>
+                     
+                 
+                   </div>
                 </div>
             </div>
          </section>
