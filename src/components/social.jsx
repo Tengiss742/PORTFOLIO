f2ca693765efcb2301f005b7da629ef0587b67b9
tengis <tengisuuganbaatar123@gmail.com>
2025-03-28 11:48:17 +0800

    v3.2

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -1,18 +1,21 @@
 import './social.css'
 import { FaFacebook } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
-import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 function Social(){
     return(
         <>
         <section id='Social'>
         <div className='miniBackground'>
-            <div>
-            <h1 className='textshuu'>Contact Me</h1> 
-            <div className='iconnn1'>
-            <FaLongArrowAltRight  />
+            <div className='sad'>
+                <div className='textContainer'>
+                <h1 className='textshuu'>Contact Me</h1> 
+                </div>
+            <div className='icons'>
+            <FaSquareInstagram />
             <FaFacebook />    
             <FaDiscord />
+            
 
             </div>
             </div>
