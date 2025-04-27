ccbc3e259f95b8fa06056bda65e4c786f3098325
tengis <tengisuuganbaatar123@gmail.com>
2025-04-27 23:06:01 +0800

    zail2

--- src/components/MultiBoxPopup.jsx
+++ src/components/MultiBoxPopup.jsx
@@ -3,9 +3,14 @@ import "./MultiBoxPopup.css";
 const popupContent = {
     1: {
       title: "Childhood",
-      content: "When I was young I live in Arvaikheer",
-      img: "https://tse4.mm.bing.net/th/id/OIP.KvyeylpC_2o0pu3GcFZP9wHaDi?rs=1&pid=ImgDetMain",
-      img: "https://tse4.mm.bing.net/th/id/OIP.ww1sXOEswcxRH6B-xQ0OFAHaGp?rs=1&pid=ImgDetMain"
+      content1: "When I was young I live in Arvaikheer. ",
+      
+      img1: "https://tse4.mm.bing.net/th/id/OIP.KvyeylpC_2o0pu3GcFZP9wHaDi?rs=1&pid=ImgDetMain",
+     
+      img2: "https://i.ytimg.com/vi/vly5WLdfss8/maxresdefault.jpg",
+      content2:"I also played with these Mortal cards a lot. While playing, I managed to collect over 300 cards."
+     
+           
     
     },
     2: {
@@ -42,18 +47,41 @@ const MultiBoxPopup = () => {
         <img src="https://www.pngall.com/wp-content/uploads/14/Gohan-PNG-File.png" alt="" className='gohan'/>
         </div>
       </div>
-      
       {activePopup && (
-        <div className="popup-overlay active">
-          <div className="popup-box">
-            <span className="close-btn" onClick={closePopup}>×</span>
-            <h2 className="sugani">{popupContent[activePopup].title}</h2>
-            <p className="eehuur">{popupContent[activePopup].content}</p>
-            <img src={popupContent[activePopup].img} alt="" width="50%" className="zurag7" />
-            <img src={popupContent[activePopup].img} alt="" width="10%" className="zurag8" />
-          </div>
-        </div>
+  <div className="popup-overlay active">
+    <div className="popup-box">
+      <span className="close-btn" onClick={closePopup}>×</span>
+      <h2 className="sugani">{popupContent[activePopup].title}</h2>
+      
+      {popupContent[activePopup].content1 && (
+        <p className="eehuur">{popupContent[activePopup].content1}</p>
       )}
+      <h1 className="mortal">{popupContent[activePopup].content2}</h1>
+      {popupContent[activePopup].img1 && (
+        <img
+          src={popupContent[activePopup].img1}
+          alt={popupContent[activePopup].title + " image 1"}
+          width="45%"
+          className="zurag7"
+        />
+      )}
+
+  
+      
+      {popupContent[activePopup].img2 && (
+        <img
+          src={popupContent[activePopup].img2}
+          alt={popupContent[activePopup].title + " image 2"}
+          width="20%"
+          className="zurag4"
+        />
+      )}
+
+   
+    </div>
+  </div>
+)}
+
         </div>
         
     </section>
